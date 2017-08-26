28ac23ffc2d8bbe0d01c2b1e9c283da281c1a5e7
Katrien.Bernaerts <katrien.bernaerts@gmail.com>
2017-08-27 00:10:25 +0200

    readme file added

--- src/main/java/contract_net/ContractNet.java
+++
@@ -1,9 +0,0 @@
-package contract_net;
-
-public class ContractNet {
-	
-	public ContractNet(){
-		//TODO?? here come all the methods that send messages of different types, REFUSE, PROPOSE, CALL_FOR_PROPOSAL, ...
-	}
-
-}
